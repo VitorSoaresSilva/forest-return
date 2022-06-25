80f88ba63fb1eb12b1c257d0322f02afb9df6e47
Vitor Soares Silva <vitrola.vitor@gmail.com>
2022-06-25 16:26:13 -0300

    Simple move Boxer

---
+++ Assets/ForestReturn/Scripts/Enemies/SimpleNavmeshAgentMove.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class SimpleNavmeshAgentMove : MonoBehaviour
+{
+    [SerializeField] private Transform[] points;
+    private NavMeshAgent _navMeshAgent;
+    private Transform _target;
+    private Animator _animator;
+    private void Awake()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.destination = points[Random.Range(0, points.Length)].position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (_animator != null)
+        {
+            _animator.SetBool("isMoving",_navMeshAgent.velocity.magnitude > 0.01f);
+        }
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+        {
+            _navMeshAgent.destination = points[Random.Range(0, points.Length)].position;
+        }
+    }
+}
